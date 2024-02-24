37b9ce034bc19894e6bc4152bfcdd5f801c21e78
WilhelmBuitrago <wibuitragog@unal.edu.co>
2024-02-24 16:45:05 -0500

    Prueba v2

--- code/prueba.py
+++ code/prueba.py
@@ -1,2 +1,18 @@
-print("Hello world")
-print("a")
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import tensorflow as tf
+import keras.layers as layers
+from keras.models import Model
+import keras.backend as K
+from keras.applications.vgg16 import VGG16
+import numpy as np
+from keras.layers import Lambda
+from tqdm import tqdm
+import random
+from Metrics import (dice,
+                     jaccard,
+                     sensitivity,
+                     specificity)
+from convRFF.train import get_compile_parameters
+import pandas as pd

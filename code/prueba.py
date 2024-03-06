8d92c23ac076e37f03be499a07d3a030ace04616
SergioAGC2001 <seguzmanc@unal.edu.co>
2024-03-06 11:28:23 -0500

    Upload Model and Test GPU

--- code/prueba.py
+++ code/prueba.py
@@ -1,4 +1,5 @@
 from tensorflow.python.client import device_lib
+import tensorflow
 
 
 def get_available_devices():
@@ -7,3 +8,11 @@ def get_available_devices():
 
 
 print(get_available_devices())
+
+gpus = tensorflow.config.list_physical_devices('GPU')
+if gpus:
+    device = '/device:GPU:0'
+else:
+    device = '/device:CPU:0'
+with tensorflow.device(device):
+    print("device:", device)
